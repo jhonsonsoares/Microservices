c0d66d47f446baf64613e9aba84de818ee0dbd72
jhonsonsoares <80490109+jhonsonsoares@users.noreply.github.com>
2024-10-27 14:58:37 -0300

    criação dos repositórios

---
+++ Catalog.API/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
+
+namespace Catalog.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+            });
+
+        }
+    }
+}
