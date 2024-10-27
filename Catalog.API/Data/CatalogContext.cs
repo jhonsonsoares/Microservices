3f4a5384c0190d24a479784c7d22ea62c0a52724
jhonsonsoares <80490109+jhonsonsoares@users.noreply.github.com>
2024-10-27 13:11:16 -0300

    Inicio do Desenvolvimento

---
+++ Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configurarion) 
+        {
+            var client = new MongoClient(configurarion.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var database = client.GetDatabase(configurarion.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configurarion.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "1233444444",
+                    Name = "Name",
+                    Description = "Description",
+                    Image = "teste.png",
+                    Price = 10,
+                    Category = "Category"
+                }
+            };
+        }
+
+    }
+}
